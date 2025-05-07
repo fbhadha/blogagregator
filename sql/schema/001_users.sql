2985a62fce8ce47b238012af48931e1853074c1b
fbhadha <fbhadha@blindword.com>
2025-05-07 10:53:13 -0400

    breaking apart handlers and commands into files

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 CREATE TABLE users (
     id UUID PRIMARY KEY,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    name TEXT NOT NULL
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT UNIQUE NOT NULL 
 );
 
 -- +goose Down
